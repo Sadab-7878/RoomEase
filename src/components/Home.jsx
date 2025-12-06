6988dbe0fa18cc90ea1080178c5078b5e686fb0c
jsami31 <57608969+Sadab-7878@users.noreply.github.com>
2025-12-06 12:44:33 +0600

    1
    1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { IoHome } from "react-icons/io5";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>
+        {/* 1 */}
+        <div className="h-[100px] bg-[#99F2CE]">
+          <div>
+            <div className="flex items-center gap-2">
+              <IoHome size={32} />
+              <p className="text-3xl font-bold">RoomEase</p>
+            </div>
+            <div></div>
+          </div>
+          <div></div>
+        </div>
 
-export default Home
+        {/* 2 */}
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
